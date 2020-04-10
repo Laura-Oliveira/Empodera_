0a27682d1d44e6a2cdb45bbe8749bd55b8bb77e8
Laura-Oliveira <laura.regina.cp@gmail.com>
2020-04-10 18:22:18 -0300

    Layout implemention of the page register user

--- Empodera/app/src/main/java/com/empodera/LoginActivity.java
+++ Empodera/app/src/main/java/com/empodera/LoginActivity.java
@@ -2,13 +2,23 @@ package com.empodera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void openRegisterUserPage(View view)
+    {
+        Intent activity = new Intent(LoginActivity.this, RegisterUserActivity.class);
+        startActivity(activity);
     }
 }
